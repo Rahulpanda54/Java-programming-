1333f0c48903107eaa736c970aaa0f7f729f83b8
Rahul Panda <rahulpanda7851@gmail.com>
2026-05-23 10:57:19 +0530

    Add files via upload

---
+++ switches.java
@@ -0,0 +1,24 @@
+public class switches {
+    public static void main(String[] args){
+
+        String day = "tuesday";
+
+        switch(day){
+
+            case "monday" :
+                System.out.println("This is monday  : ");
+                break;
+            case "tuesday" :
+                System.out.println("This is tuesday :");
+                break;
+            case "wednesday":
+                System.out.println("This is wednesday:");
+                break;
+            case "thrusday" :
+                System.out.println("This is thrusday :");
+                break;
+            default:
+                System.out.println(" Nothing :");
+        }
+    }
+}
